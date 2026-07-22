a227b4626f8cd9ac8852e98028c619231d2336b0
Alvendher Joy Francisco <alvendherfrancisco01@gmail.com>
2026-07-22 13:51:21 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import MyBooths from '@/pages/MyBooths';
 import PrintShop from '@/pages/PrintShop';
 import Notifications from '@/pages/Notifications';
 import Profile from '@/pages/Profile';
+import Orders from '@/pages/Orders';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
@@ -47,6 +48,7 @@ const AuthenticatedApp = () => {
         <Route path="/booth" element={<Booth />} />
         <Route path="/my-booths" element={<MyBooths />} />
         <Route path="/print-shop" element={<PrintShop />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/notifications" element={<Notifications />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
