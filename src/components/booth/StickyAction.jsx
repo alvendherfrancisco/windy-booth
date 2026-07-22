681f6c9e25602aeb263b7088f3c9e18b0d22b0ad
Alvendher Joy Francisco <alvendherfrancisco01@gmail.com>
2026-07-22 17:02:23 +0000

    Visual edits

--- src/components/booth/StickyAction.jsx
+++ src/components/booth/StickyAction.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function StickyAction({ children }) {
   return (
     <div className="fixed bottom-16 left-0 right-0 z-40 border-t border-[#E8E2D8] bg-[#F9F7F2] md:bottom-0 md:left-60">
-      <div className="mx-auto max-w-5xl px-4 py-4 md:px-10">
+      <div className="mx-auto max-w-5xl px-4 py-4 md:px-10 bg-[hsl(var(--background))]">
         <div className="mx-auto max-w-4xl">{children}</div>
       </div>
-    </div>
-  );
+    </div>);
+
 }
